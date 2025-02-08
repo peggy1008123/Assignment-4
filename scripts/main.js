fba6f5962678724c5a5a7769ce95163950f01e8f
peggy1008123 <71455962+peggy1008123@users.noreply.github.com>
2025-02-08 10:53:03 -0500

    move the file out

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+ScrollReveal().reveal('.card.Sun');
+ScrollReveal().reveal('.card.Mercury', { delay: 100 });
+ScrollReveal().reveal('.card.Venus', { delay: 200 });
+ScrollReveal().reveal('.card.Earth', { delay: 300 });
+ScrollReveal().reveal('.card.Mars', { delay: 400 });
+ScrollReveal().reveal('.card.Jupiter', { delay: 500 });
+ScrollReveal().reveal('.card.Sarturn', { delay: 600 });
+ScrollReveal().reveal('.card.Uranus', { delay: 700 });
+ScrollReveal().reveal('.card.Neptune', { delay: 800 });
