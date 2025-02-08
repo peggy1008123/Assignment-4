f999a5592a01276c5f0423e6372eeab797de2e2a
peggy1008123 <71455962+peggy1008123@users.noreply.github.com>
2025-02-08 10:20:26 -0500

    finished

--- .eleventy.js
+++ .eleventy.js
@@ -3,4 +3,11 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
+  eleventyConfig.addShortcode("card", function(classname, title, content) {
+    return `<div class="card ${title}">
+      <div class="planet ${classname}"></div>
+      <h2 class="${title}">${title}</h2>
+      <p class="${content}">${content}</p>
+    </div>`;
+  });
 };
